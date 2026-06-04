0633986b8e0d9134802e204416f3d35a86c288bd
Brandon <bcalero234@gmail.com>
2026-06-04 08:40:37 -0400

    Set up LoadData class init in order to store variables depending on what type of data is used

--- src/Load.py
+++ src/Load.py
@@ -1,5 +1,10 @@
 import pandas as pd
-import csv
 import json
 import requests
 
+class LoadData:
+    def __init__(self):
+        self.csv_data = None
+        self.json_data=None
+        self.api_data=None
+    
