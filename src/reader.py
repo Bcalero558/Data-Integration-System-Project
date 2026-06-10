b11e9c7d536deb2bf3eee9d37363e291e93fb8c1
Brandon <bcalero234@gmail.com>
2026-06-10 12:31:49 -0400

    added comments to describe what class does

--- src/reader.py
+++ src/reader.py
@@ -1,13 +1,26 @@
 import pandas as pd
 import json
 import requests
+import logging
 
 class ReadData:
-    def __init__(self):
+    logger = None
+    def __init__(self,log_file):
         self.csv_data = None
         self.json_data=None
         self.api_data=None
+        self.logger = logging.get_logger(log_file)
     
+    
+    logger.setLevel(logging.DEBUG)
+
+
+
     def read_csv(self,filename):
-        
+        try:
+            csv_data = pd.read_csv(filename)
+        except Exception as e:
+            self.logger.Error("Error: Reading CSV File")
+        else:
+            return csv_data
     
