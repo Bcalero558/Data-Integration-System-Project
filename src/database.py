51f441144973e7cae6c76259d99707c4ef0b5702
Brandon <bcalero234@gmail.com>
2026-06-11 11:17:02 -0400

    added function to create table

--- src/database.py
+++ src/database.py
@@ -23,5 +23,33 @@ class DatabaseOperations:
         except Exception as e:
             print("Failed to Connect")
 
+    #Create table
+    def create_table(conn):
+        cursor = conn.cursor()
+        cursor.execute(
+        """
+            CREATE TABLE IF NOT EXIST members(
+            id SERIAL PRIMARY KEY,
+            age INT NOT NULL
+            gender VARCHAR(6)
+            weight DECIMAL(4,1),
+            height DECIMAL(3,2),
+            max_bpm INT NOT NULL
+            avg_bpm INT NOT NULL
+            resting_bpm INT NOT NULL
+            session_duration DECIMAL(3,2)
+            calories_burned DECIMAL(5,1)
+            workout_type VARCHAR(255)
+            fat_percentage DECIMAL(3,1)
+            water_intake DECIMAL(2,1)
+            Workout_Frequency INT NOT NULL
+            experience_level INT NOT NULL
+            BMI DECIMAL(4,2)
+            )
+
+        """
+        )
+        conn.commit()
+        print("Created Table Successfully")
     
 
