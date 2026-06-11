04447e737278cc0f0795166664cb43ca9f0375b7
Brandon <bcalero234@gmail.com>
2026-06-11 12:37:20 -0400

    insert function command

--- src/database.py
+++ src/database.py
@@ -9,6 +9,8 @@ class DatabaseOperations:
     def __init__(self, data):
         self.data = data
 
+    #connect to postgres
+    #todo:
     def connect_to_database():
         try:
             conn = psycopg2.connect(
@@ -44,12 +46,47 @@ class DatabaseOperations:
             water_intake DECIMAL(2,1)
             Workout_Frequency INT NOT NULL
             experience_level INT NOT NULL
-            BMI DECIMAL(4,2)
+            bmi DECIMAL(4,2)
             )
 
         """
         )
         conn.commit()
         print("Created Table Successfully")
-    
+        
+    #add elements to table
+    def insert_data(self,conn):
+        data_list = self.data.list()
+        query = """
+        INSERT INTO members (
+        age,gender,weight,height,max_bpm,avg_bpm,resting_bpm,session_duration,
+        calories_burned,workout_type,fat_percentage,water_intake,Workout_Frequency,
+        experience_level,bmi) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+        """
+
+        with conn.cursor() as cursor:
+            cursor.executemany(
+                query,
+                [
+                    (
+                        row["Age"],
+                        row["Gender"],
+                        row["Weight (kg)"],
+                        row["Height (m)"],
+                        row["Max_BPM"],
+                        row["Avg_BPM"],
+                        row["Resting_BPM"],
+                        row["Session_Duration (hours)"],
+                        row["Calories_Burned"],
+                        row["Workout_Type"],
+                        row["Fat_Percentage"],
+                        row["Water_Intake (liters)"],
+                        row["Workout_Frequency (days/week)"],
+                        row["Experience_Level"],
+                        row["BMI"]
+                    )
+                    for row in data_list
+                ],
+            )
+        conn.commit()
 
