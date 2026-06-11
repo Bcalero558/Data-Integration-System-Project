2653d4ca8eeeb3ac7a21a6e915f1d1dc76ed321f
Brandon <bcalero234@gmail.com>
2026-06-11 10:11:37 -0400

    reset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from src import reader
+from src import reader, clean, validate 
 
 reading = reader.ReadData("ETL.log")
 data = reading.read_csv("data\\raw\\gym_members_exercise_tracking.csv")
 
-print(data.info)
+print(data)
