b35fd6467afba434cf1c9e812857850246d697c6
Brandon <bcalero234@gmail.com>
2026-06-15 11:05:06 -0400

    added verififcation

--- src/validate.py
+++ src/validate.py
@@ -1,9 +1,26 @@
 import pandas as pd
 
-"""Removes invalid data and stores away elsewhere such as 
+"""TODO:
+    Removes invalid data and stores away elsewhere such as 
     Max_BPM < 220 
     resting bpm > 30
     age < 100
     Gender == Male || Female
     No duplicates
 """
+class Validator:
+    data = pd.DataFrame()
+    def __init__(self,df):
+        self.data= df.copy
+        
+    # TODO:cleans and formats data for proper use
+    def validate_rows(self, condition,data) :
+
+        accepted_row = data
+        rejected_row = pd.DataFrame()
+        for i in condition:
+            rejected_row = rejected_row.append(accepted_row[~i], ignore_index = True)
+            accepted_row = accepted_row[i]
+        
+        accepted_row.to_csv('data\\processed\\clean_data.csv')
+        rejected_row.to_csv('data\\processed\\rejected_data.csv')
