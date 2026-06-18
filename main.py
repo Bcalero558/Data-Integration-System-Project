b15037ac3e01c4ac179c7e68a7188a18d725f92d
Brandon <bcalero234@gmail.com>
2026-06-18 09:19:24 -0400

    changed variables to have gm prefix which stands for gym member as a way to organize in case of using more csv files

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 import psycopg2
-from src import reader, clean, validate ,database
-#a list to store the format of the csv file 
-format_str = ["Age","Gender","Weight (kg)","Height (m)","Max_BPM","Avg_BPM","Resting_BPM","Session_Duration (hours)","Calories_Burned","Workout_Type","Fat_Percentage","Water_Intake (liters)","Workout_Frequency (days/week)","Experience_Level","BMI"]
-#name of the file
-file_name = "data\\raw\\gym_members_exercise_tracking.csv"
-reading = reader.ReadData("ETL.log")
-data = reading.read_csv(file_name,format_str)
-#a list of conditionals saved as a string in order to use in the validator to save reason of rejection
-conditions = ["data[\"Age\"] < 100" , "data[\"Max_BPM\"] < 220" , "data[\"Resting_BPM\"] > 30"]
-validator = validate.Validator(data)
+from src import visualize, reader, validate ,database
 
-validator.validate_rows(conditions,data)
-data = reading.read_csv("data\\processed\\clean_data.csv",format_str)
+def main():
+    #a list to store the format of the csv file 
+    gm_format_str = ["Age","Gender","Weight (kg)","Height (m)","Max_BPM","Avg_BPM","Resting_BPM","Session_Duration (hours)","Calories_Burned","Workout_Type","Fat_Percentage","Water_Intake (liters)","Workout_Frequency (days/week)","Experience_Level","BMI"]
+    #name of the file
+    gm_file_name = "data\\raw\\gym_members_exercise_tracking.csv"
+    gm_reading = reader.ReadData("ETL.log")
+    gm_data = gm_reading.read_csv(gm_file_name,gm_format_str)
+    #a list of conditionals saved as a string in order to use in the validator to save reason of rejection
+    gm_conditions = ["data[\"Age\"] < 100" , "data[\"Max_BPM\"] < 220" , "data[\"Resting_BPM\"] > 30"]
+    gm_validator = validate.Validator(gm_data)
 
-transfer = database.DatabaseOperations(data)
-db = transfer.connect_to_database()
-transfer.create_table(db)
-transfer.insert_data(db)
+    gm_validator.validate_rows(gm_conditions,gm_data)
+    gm_data = gm_reading.read_csv("data\\processed\\clean_data.csv",format_str)
 
-print(data)
+    gm_transfer = database.DatabaseOperations(gm_data)
+    gm_db = gm_transfer.connect_to_database()
+    gm_transfer.create_table(gm_db)
+    gm_transfer.insert_data(gm_db)
+
+    query = gm_transfer.query(gm_db)
+    visualize.DataWindow(query)
+
+
+    print(query)
+if __name__ == "__main__":
+    main()
