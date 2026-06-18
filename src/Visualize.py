053f268fb607aca760c58bc0e6e9f626c3c131c8
Brandon <bcalero234@gmail.com>
2026-06-18 11:16:47 -0400

    reset on visuals

--- src/Visualize.py
+++ src/Visualize.py
@@ -1,12 +1,1 @@
 import pandas as pd
-
-class Cleaner:
-    
-    data = pd.DataFrame()
-    def __init__(self,df :pd.DataFrame):
-        self.data= df.copy
-        
-
-    
-    def remove_duplicates():
-        pass
